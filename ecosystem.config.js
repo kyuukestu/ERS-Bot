25587107c50aaf722500c38de0cd0b07799cb862
J. A-L G. P. <kyuukestu@gmail.com>
2025-10-04 03:07:46 -0500

    testing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,17 @@
-const config: { apps: any[] } = {
+const config = {
 	apps: [
 		{
 			name: 'ERS-bot',
 			script: 'src/index.ts',
-			interpreter: 'ts-node',
+			interpreter: './node_modules/.bin/ts-node',
 			watch: true,
 			ignore_watch: ['node_modules', '.git', 'logs', '.DS_Store'],
-			watch_options: {
-				usePolling: true, // needed for macOS file system notifications
-			},
+			watch_options: { usePolling: true },
 			cron_restart: '0 6 * * *',
 			restart_delay: 10000,
 			max_memory_restart: '200M',
+			out_file: 'logs/out.log',
+			error_file: 'logs/error.log',
 		},
 	],
 };
