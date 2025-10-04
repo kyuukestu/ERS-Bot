83ca39f72f5109c0f49e35272bf2a82b6ffd7c3d
J. A-L G. P. <kyuukestu@gmail.com>
2025-10-04 02:20:37 -0500

    automation edits

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -4,6 +4,11 @@ const config: { apps: any[] } = {
 			name: 'ERS-bot',
 			script: 'src/index.ts',
 			interpreter: 'ts-node',
+			watch: true,
+			ignore_watch: ['node_modules', '.git', 'logs', '.DS_Store'],
+			watch_options: {
+				usePolling: true, // needed for macOS file system notifications
+			},
 			cron_restart: '0 6 * * *',
 			restart_delay: 10000,
 			max_memory_restart: '200M',
