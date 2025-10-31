33a1f13d6405c3a5c5e3eea26058818fa02001b7
J. A-L G. P. <kyuukestu@gmail.com>
2025-10-30 19:35:08 -0500

    Allow to run without DB

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,6 @@ const connectDB = async () => {
 		console.log('✅ Connected to MongoDB');
 	} catch (error) {
 		console.error('❌ Error connecting to MongoDB:', error);
-		process.exit(1); // Exit with failure status code
 	}
 };
 
